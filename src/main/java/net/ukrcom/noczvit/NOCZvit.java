40c9cd5d9f87c940ee9fbff8c721a4a0b99b3d9c
Alexander Russkih <oldengremlin@gmail.com>
2025-05-07 09:16:29 +0300

    Initial commit

---
+++ src/main/java/net/ukrcom/noczvit/NOCZvit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.ukrcom.noczvit;
+
+/**
+ *
+ * @author olden
+ */
+public class NOCZvit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
